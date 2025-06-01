1b4504cb0b26c1aedfcbe701feffcaabf2f3946a
mahmoudmostafahmed <99724533+mahmoudmostafahmed@users.noreply.github.com>
2025-06-01 12:40:09 +0300

    Update CheckoutOverviewPage.java

--- src/test/java/com/Automationtask/saucedemosite/screen/CheckoutOverviewPage.java
+++ src/test/java/com/Automationtask/saucedemosite/screen/CheckoutOverviewPage.java
@@ -3,10 +3,10 @@ package com.Automationtask.saucedemosite.screen;
 import org.openqa.selenium.By;
 
 public class CheckoutOverviewPage extends BaseScreen {
-    private final By summaryTotal = By.className("summary_total_label");
+    private static final By summaryTotal = By.className("summary_total_label");
     private final By finishBtn = By.id("finish");
 
-    public boolean isOrderSummaryVisible() {
+    public static boolean isOrderSummaryVisible() {
         return waitUtils.waitUntilElementIsVisible(summaryTotal).isDisplayed();
     }
 
