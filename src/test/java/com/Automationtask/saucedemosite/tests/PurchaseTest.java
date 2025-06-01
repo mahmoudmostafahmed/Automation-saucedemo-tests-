ed1a5ed588ca74b503f2f5fd3015b993c5e1c699
mahmoudmostafahmed <99724533+mahmoudmostafahmed@users.noreply.github.com>
2025-06-01 12:14:19 +0300

    Update PurchaseTest.java

--- src/test/java/com/Automationtask/saucedemosite/tests/PurchaseTest.java
+++ src/test/java/com/Automationtask/saucedemosite/tests/PurchaseTest.java
@@ -1,53 +1,53 @@
 package com.Automationtask.saucedemosite.tests;
 
 import com.Automationtask.saucedemosite.screen.*;
+import com.Automationtask.saucedemosite.tests.UserDataFetcher;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
+import static com.Automationtask.saucedemosite.utils.read_properties.ReadProperties.setSauceCorporationConfig;
+
 public class PurchaseTest extends BaseTest {
+
+    String userName = setSauceCorporationConfig().getProperty("userName");
+    String password = setSauceCorporationConfig().getProperty("password");
+
+    public PurchaseTest() throws IOException {
+    }
 
     @Test
     public void completePurchaseFlow() throws Exception {
-     
+        // Initialize Page Objects
         LoginPage loginPage = new LoginPage();
-        loginPage.enterUsername("standard_user")
-                .enterPassword("secret_sauce")
+        InventoryPage inventoryPage = new InventoryPage();
+        ProductPage productPage = new ProductPage();
+        CartPage cartPage = new CartPage();
+        CheckoutPage checkoutPage = new CheckoutPage();
+        CheckoutOverviewPage overviewPage = new CheckoutOverviewPage();
+        CheckoutCompletePage completePage = new CheckoutCompletePage();
+
+        browser.SauceDemo.loginPage.enterUsername(userName)
+                .enterPassword(password)
                 .clickOnSubmitButton();
 
-       
-        InventoryPage inventoryPage = new InventoryPage();
         inventoryPage.addFleeceJacketToCart();
-
-        
         inventoryPage.openOnesieDetails();
-        ProductPage detailPage = new ProductPage();
-        detailPage.addToCart();
-        detailPage.goBackToInventory();
-
- 
-        Assert.assertEquals(inventoryPage.getCartBadgeCount(), "2");
-
- 
+        productPage.addToCart();
+        productPage.goBackToInventory();
+        Assert.assertEquals(inventoryPage.getCartBadgeCount(), "2", "Cart badge count mismatch");
         inventoryPage.goToCart();
-        CartPage cartPage = new CartPage();
-        Assert.assertTrue(cartPage.isProductInCart("Sauce Labs Fleece Jacket"));
-        Assert.assertTrue(cartPage.isProductInCart("Sauce Labs Onesie"));
+        Assert.assertTrue(cartPage.isProductInCart("Sauce Labs Fleece Jacket"), "Fleece Jacket not in cart");
+        Assert.assertTrue(cartPage.isProductInCart("Sauce Labs Onesie"), "Onesie not in cart");
         cartPage.clickCheckout();
-
-     
         String[] userData = UserDataFetcher.fetchRandomUserData();
-        CheckoutPage checkoutPage = new CheckoutPage();
         checkoutPage.enterFirstName(userData[0])
                 .enterLastName(userData[1])
                 .enterPostalCode(userData[2])
                 .clickContinue();
-
-        CheckoutOverviewPage overviewPage = new CheckoutOverviewPage();
-        Assert.assertTrue(overviewPage.isOrderSummaryVisible());
+        Assert.assertTrue(overviewPage.isOrderSummaryVisible(), "Order summary is not visible");
         overviewPage.clickFinish();
-
-      
-        CheckoutCompletePage completePage = new CheckoutCompletePage();
-        Assert.assertTrue(completePage.isOrderCompleteMessageVisible());
+        Assert.assertTrue(completePage.isOrderCompleteMessageVisible(), "Order completion message not visible");
     }
 }
